2e4b58beb70be8dcdfcdbd34abf2b481473369a0
Mark <markcfleming1990@gmail.com>
2022-01-30 13:54:58 -0600

    cleaning up leftover for reset

--- app.js
+++ app.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-const inquirer = require('inquirer');
-
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
-  ]);
-};
-
-
-
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
-
-//const pageHTML = generatePage(name, github);
-
-//console.log(profileDataArgs);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
-
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
@@ -54,4 +16,3 @@ fs.writeFile('./index.html', pageHTML, err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
->>>>>>> develop
